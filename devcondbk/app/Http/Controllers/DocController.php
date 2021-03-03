287f28cf226ea1e83112e957b5dad4ca3a5fcd1b
Ivan dos santos <ivantkd1@gmail.com>
2021-03-03 18:27:46 -0300

    API Billet and Docs

--- devcondbk/app/Http/Controllers/DocController.php
+++ devcondbk/app/Http/Controllers/DocController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+use App\Models\Doc;
 
 class DocController extends Controller
 {
-    //
+    public function getAll() {
+        $array = ['error' => ''];
+
+        $docs = Docs::all();
+
+        foreach($docs as $docKey => $docValue) {
+           $docs[$docKey]['fileurl'] = asset('storage/'.$docValue['fileurl']); 
+        }
+        $array['list'] = $docs;
+
+        return $array;
+    }
 }
